b4cab71344cf09be6e3896ae5925d1b086103f22
minsung.jung <jmstd0618@naver.com>
2019-05-19 00:08:32 +0900

    test

--- jms/printStart.py
+++ jms/printStart.py
@@ -1,3 +1,17 @@
 import adder
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
 print(adder.add(5,50))
 print(adder.add(100,200))
+def a(a,b):
+    if(a>b):
+        print("a is bigger than b")
+    else:
+        print("a즐")
+
+a(10,20)
+a(20,1)
